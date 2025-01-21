ada0d4918914c56f86ec6b13c17615a758428391
desenvolvimentogrupopixelpoint <desenvolvimento@grupopixelpoint.com>
2025-01-21 15:07:05 -0300

    play_videos.py

--- play_videos.py
+++ play_videos.py
@@ -56,7 +56,7 @@ def play_all_videos_in_loop():
             for file in files_in_active:
                 video_path = os.path.join(UPLOAD_FOLDER, file)
                 subprocess.Popen([
-                    "mpv", "--fs", "--no-audio", "--quiet", "--vo=drm", "--vf=scale=640:360", 
+                    "mpv", "--fs", "--no-audio", "--quiet", "--vo=drm", 
                     "--framedrop=vo", "--speed=1.1", "--loop", video_path
                 ], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL).wait()
         else:
