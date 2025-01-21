efad985a90c9372d314af50681eff392b09c8af3
desenvolvimentogrupopixelpoint <desenvolvimento@grupopixelpoint.com>
2025-01-20 23:32:32 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -24,11 +24,19 @@ pip3 install flask || { echo "Erro ao instalar Flask. Finalizando a instalação
 echo "Criando pastas e configurando permissões..."
 mkdir -p ~/.config/mpv
 chmod -R 777 ~/.config/mpv
+mkdir -p /home/videos
+mkdir -p /home/midias_inativas
 mkdir -p /home/pixelpoint/templates
-mkdir -p /home/pixelpoint/videos
-mkdir -p /home/pixelpoint/midias_inativas
-mkdir -p /home/pixelpoint
+chmod -R 777 /home/videos
+chmod -R 777 /home/midias_inativas
 chmod -R 777 /home/pixelpoint
+
+# Movendo a pasta templates para pixelpoint
+echo "Movendo a pasta templates para /home/pixelpoint..."
+if [ -d "/home/templates" ]; then
+    mv /home/templates /home/pixelpoint/templates || { echo "Erro ao mover a pasta templates"; exit 1; }
+fi
+chmod -R 777 /home/pixelpoint/templates
 
 # Criando o arquivo mpv.conf
 echo "Configurando mpv.conf..."
@@ -39,18 +47,18 @@ gpu-context=x11
 EOF
 
 # Movendo arquivos para os diretórios
-echo "Movendo arquivos para os diretórios..."
-curl -fsSL https://raw.githubusercontent.com/desenvolvimentogrupopixelpoint/pixel_play.1.0/main/Logo.png -o /home/pixelpoint/Logo.png || { echo "Erro ao baixar Logo.png"; exit 1; }
+echo "Baixando e movendo arquivos para os diretórios..."
+curl -fsSL https://raw.githubusercontent.com/desenvolvimentogrupopixelpoint/pixel_play.1.0/main/Logo.png -o /home/Logo.png || { echo "Erro ao baixar Logo.png"; exit 1; }
 curl -fsSL https://raw.githubusercontent.com/desenvolvimentogrupopixelpoint/pixel_play.1.0/main/templates/Index.html -o /home/pixelpoint/templates/Index.html || { echo "Erro ao baixar Index.html"; exit 1; }
 curl -fsSL https://raw.githubusercontent.com/desenvolvimentogrupopixelpoint/pixel_play.1.0/main/templates/logop.png -o /home/pixelpoint/templates/logop.png || { echo "Erro ao baixar logop.png"; exit 1; }
 curl -fsSL https://raw.githubusercontent.com/desenvolvimentogrupopixelpoint/pixel_play.1.0/main/play_videos.py -o /home/pixelpoint/play_videos.py || { echo "Erro ao baixar play_videos.py"; exit 1; }
-echo "{}" > /home/pixelpoint/metadata.json
+echo "{}" > /home/metadata.json
 
 # Configurando rc.local
 echo "Configurando rc.local..."
 cat <<EOF > /etc/rc.local
 #!/bin/bash
-(sleep 4 && fim -q -a /home/pixelpoint/Logo.png) &
+(sleep 4 && fim -q -a /home/Logo.png) &
 exit 0
 EOF
 chmod +x /etc/rc.local
